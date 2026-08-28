18bbfa3d8d51e1980c8fdbfab88ec93a1ec797d7
Dsamofalov <25666939+Dsamofalov@users.noreply.github.com>
2026-08-28 11:03:57 +0300

    feat: define signed registered-peer attestation

--- contracts/interfaces/IMerzavetsWorld.sol
+++ contracts/interfaces/IMerzavetsWorld.sol
@@ -18,5 +18,23 @@ interface IMerzavetsWorld {
         uint256 deadline;
     }
 
+    struct PeerAttestation {
+        address actorWallet;
+        uint256 actorTokenId;
+        address peerWallet;
+        uint256 peerTokenId;
+        uint256 chainId;
+        uint64 blockNumber;
+        bytes32 encounterDigest;
+        uint256 nonce;
+        uint256 deadline;
+    }
+
     function applyVerifiedActivity(ActivityAttestation calldata attestation) external;
+
+    function applyVerifiedPeerContact(
+        uint256 actorTokenId,
+        uint256 peerTokenId,
+        bytes32 encounterDigest
+    ) external;
 }
